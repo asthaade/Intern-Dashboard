05cac26c87fc2faac4d176f072ed20aca134025a
asthaade <adeastha398>
2025-08-03 12:14:42 +0530

    V2

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/internDashboardDB';
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // --- MongoDB Connection ---
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
